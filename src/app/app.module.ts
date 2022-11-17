1c5d6bbbff3d0384484cf9706ad99f10d6b91609
sandeep100690 <sandeepamali10@gmail.com>
2022-11-17 06:46:45 +0530

    2.1 templates and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    declarations: [ 
+      AppComponent, 
+      HelloComponent, 
+      ServerComponent
+    ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule
+   ],
+  bootstrap:    [AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-server",
+  templateUrl: 'server.component.html',
+  // styleUrls: ["./server.component.css"]
+  styles: [`
+    h2{
+      color: blue;
+    }
+  `]
+})
+
+export class ServerComponent {
+
+}
