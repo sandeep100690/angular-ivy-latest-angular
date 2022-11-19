46acefaac97641825fda6492b492bc19ed2fd18e
sandeep100690 <sandeepamali10@gmail.com>
2022-11-19 06:41:46 +0530

    disabled property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   constructor() { 
     setTimeout(()=>{
-      
+      this.allowNewServer = true;
     }, 2000)
   }
 
