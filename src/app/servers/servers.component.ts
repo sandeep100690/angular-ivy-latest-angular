dc3cbdc813e71755a3de2f8750c16b7b3716edac
sandeep100690 <sandeepamali10@gmail.com>
2022-11-19 06:35:49 +0530

    property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer: boolean = false;
+  constructor() { 
+    setTimeout(()=>{
+      
+    }, 2000)
+  }
 
   ngOnInit() {
+
   }
 
 }
